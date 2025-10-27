df2c1282d3ae01e3f1af76bf63d849c42fd9c3a7
YueMiyuki <miraclecutelemon@gmail.com>
2025-10-27 15:11:41 +0800

    [experimental] enhancement(server): Use reactCompiler

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
+  reactCompiler: true,
   experimental: {
     cssChunking: "strict",
     useCache: true,
