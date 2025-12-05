a1ac4e6b1eb0df07ec9140ec9f2cf44b3bef8d16
Miyuki <76854136+YueMiyuki@users.noreply.github.com>
2025-12-05 09:08:13 +0800

    Remove modularizeImports for next.config.ts
    Removed modularizeImports configuration for lucide-react.

--- next.config.ts
+++ next.config.ts
@@ -32,11 +32,6 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
   },
-  modularizeImports: {
-    "lucide-react": {
-      transform: "lucide-react/dist/esm/icons/{{kebabCase member}}",
-    },
-  },
   async headers() {
     return [
       {
